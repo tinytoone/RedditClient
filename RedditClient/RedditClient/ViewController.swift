fb7f8134aa954ffdf9350c0d4c17b9d17eb2f985
tinytoone <a.voitsekhivskyi@gmail.com>
2019-10-27 13:25:05 +0200

    adding empty project

---
+++ RedditClient/RedditClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedditClient
+//
+//  Created by Anton Voitsekhivskyi on 10/27/19.
+//  Copyright © 2019 AVoitsekhivskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
